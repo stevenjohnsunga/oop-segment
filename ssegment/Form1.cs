b985e71d7905ea540fdf4a949e449639d0d35f43
Steven John Sunga <sjasunga@iskolarngbayan.pup.edu.ph>
2021-12-08 23:52:33 +0800

    added comments  so anyone who views my code will understand

--- ssegment/Form1.cs
+++ ssegment/Form1.cs
@@ -16,33 +16,25 @@ namespace ssegment
         {
             InitializeComponent();
         }
-        /* 
-           if(.Visible== true)
-           {
-               .BackColor = Color.;
-           }
-          */
-        private void ChangeEnter(Button sender)
+        private void ChangeEnter(Button sender) //function to change back color of a called button
         {
             if (sender.Visible)
             {
                 sender.BackColor = Color.Red;
             }
         }
-        private void vis(Button sen1)
+        private void vis(Button sen1) //function to disable visibility of a called button 
         {
             sen1.Visible = false;
         }
-        private void ChangeLeave(Button sender)
+        private void ChangeLeave(Button sender) //function to change the  backcolor of called button
         {
             if (sender.Visible)
             {
                 sender.BackColor = Color.Maroon;
             }
         }
-
-
-        private void grpALLTRU(GroupBox sen)
+        private void grpALLTRU(GroupBox sen) //function to enable visibility of all btns in a called groupbox
         {
             foreach (Control xx in sen.Controls)
             {
@@ -52,44 +44,32 @@ namespace ssegment
                 }
             }
         }
-
-
-
-
         private void forOne_Top_Left1_Left2_MID_BOT(Button sender, Button sender1, Button sender2, Button sender3, Button sender4)
         {
-            //top
-            sender.Visible = false;
-
-            //left1
-            sender1.Visible = false;
-
-            //left2
-            sender2.Visible = false;
-
-            //mid
-            sender3.Visible = false;
-
-            //bot
-            sender4.Visible = false;
-
+            //function to form # 1 in segments
+            vis(sender);
+            vis(sender1);
+            vis(sender2);
+            vis(sender3);
+            vis(sender4);
         }
-
-
         private void forTwo_Lef1_rig2(Button sen1, Button sen2)
         {
-            sen1.Visible = false;
-            sen2.Visible = false;
-        }
 
+            //function to form # 2 in segments
+            vis(sen1);
+            vis(sen2);
+        }
         private void forThree_lef1_lef2(Button sen1, Button sen2)
         {
+            //function to form # 3 in segments
             vis(sen1);
             vis(sen2);
         }
 
         private void forFour_top_lef2_bot(Button sen1, Button sen2, Button sen3)
         {
+            //function to form # 4 in segments
             vis(sen1);
             vis(sen2);
             vis(sen3);
@@ -97,16 +77,19 @@ namespace ssegment
 
         private void forFive_right1_lef2(Button sen1, Button sen2)
         {
+            //function to form # 5 in segments
             vis(sen1);
             vis(sen2);
         }
 
         private void forSix_right1(Button sen1)
         {
+            //function to form # 6 in segments
             vis(sen1);
         }
         private void forSeven_lef11_lef2_mid_bot(Button sen1, Button sen2, Button sen3, Button sen4)
         {
+            //function to form # 7 in segments
             vis(sen1);
             vis(sen2);
             vis(sen3);
@@ -115,60 +98,40 @@ namespace ssegment
 
         private void forNine_lef2_bot(Button sen1, Button sen2)
         {
+            //function to form #9 in segments
             vis(sen1);
             vis(sen2);
         }
 
         private void forZero_mid(Button sen1)
         {
+            //function to form # 0 in segments
             vis(sen1);
         }
-
-
+        // if you are wondering why theres  no function for 8, theres no buttons in segments needed to disable visibility
         private void Form1_Load(object sender, EventArgs e)
         {
-            // forOne_Top_Left1_Left2_MID_BOT(hr1_top_btn,hr1_left1_btn,hr1_left2_btn,hr1_mid_btn,hr1_bot_btn);
-            // forTwo_Lef1_rig2(hr2_lef1_btn,hr2_rig2_btn);
-
-            //forThree_lef1_lef2(hr1_left1_btn, hr1_left2_btn);
-
-            // forFour_top_lef2_bot(hr1_top_btn,hr1_left2_btn ,hr1_bot_btn );
-
-            // forFive_right1_lef2(hr1_right1_btn, hr1_left2_btn);
-
-
+         //codes to run when program starts
         }
 
         private void hr1_top_btn_MouseEnter(object sender, EventArgs e)
         {
-            if (hr1_top_btn.Visible == true)
-            {
-                hr1_top_btn.BackColor = Color.Red;
-            }
+            ChangeEnter(hr1_top_btn);
         }
 
         private void hr1_top_btn_MouseLeave(object sender, EventArgs e)
         {
-            if (hr1_top_btn.Visible == true)
-            {
-                hr1_top_btn.BackColor = Color.Maroon;
-            }
+            ChangeLeave(hr1_top_btn);
         }
 
         private void hr1_left1_btn_MouseEnter(object sender, EventArgs e)
         {
-            if (hr1_left1_btn.Visible == true)
-            {
-                hr1_left1_btn.BackColor = Color.Red;
-            }
+            ChangeEnter(hr1_left1_btn);
         }
 
         private void hr1_left1_btn_MouseLeave(object sender, EventArgs e)
         {
-            if (hr1_left1_btn.Visible == true)
-            {
-                hr1_left1_btn.BackColor = Color.Maroon;
-            }
+            ChangeLeave(hr1_left1_btn);
         }
 
 
@@ -222,19 +185,19 @@ namespace ssegment
         {
             ChangeLeave(hr1_left2_btn);
         }
-        int cnt = 0;
-        string mysec;
-        int scs = 0;
+        int cnt = 0;  //counter
+        string mysec; //date reader
+        int scs = 0;  //stores the value of time(hh,mm,ss)
         private void timer1_Tick(object sender, EventArgs e)
         {
-            cnt += 1;
-            ss2();
-            ss1();
+            cnt += 1; //increment
+            ss2(); //calls the function for second column for seconds
+            ss1(); //calls the function for first column for seconds
         }
-        private void ss2()
+        private void ss2() //function for second column for seconds
         {
-            mysec = DateTime.Now.ToString("ss");
-            scs = Convert.ToInt32(mysec.Substring(mysec.Length - 1));
+            mysec = DateTime.Now.ToString("ss"); //stores the seconds in mysec variable
+            scs = Convert.ToInt32(mysec.Substring(mysec.Length - 1)); //gets the last char of string
             if (scs == 0)
             {
                 grpALLTRU(groupBox5);
@@ -285,51 +248,7 @@ namespace ssegment
                 forNine_lef2_bot(ss2l2, ss2bot);
             }
         }
-        /*
-          if (scs == 0)
-            {
-               
-            }
-            else if (scs == 1)
-            {
-               
-            }
-            else if (scs == 2)
-            {
-               
-            }
-            else if (scs == 3)
-            {
-               
-            }
-            else if (scs == 4)
-            {
-                
-            }
-            else if (scs == 5)
-            {
-               
-            }
-            else if (scs == 6)
-            {
-                
-            }
-            else if (scs == 7)
-            {
-                
-            }
-            else if (scs == 8)
-            {
-                grpALLTRU(groupBox);
-            }
-            else if (scs == 9)
-            {
-               
-            }
-         
-         */
-
-        private void ss1()
+        private void ss1() //function for first column for seconds
         {
             mysec = DateTime.Now.ToString("ss");
             scs= Convert.ToInt32(mysec.Substring(0, 1));
@@ -382,6 +301,30 @@ namespace ssegment
                 grpALLTRU(groupBox6);
                 forNine_lef2_bot(ss1l2, ss1bot);
             }
-        } 
         }
+        private void mm1() //function for firstt column for minss
+        {
+            mysec = DateTime.Now.ToString("mm");
+            scs = Convert.ToInt32(mysec.Substring(mysec.Length-1));
+        }
+        
+        private void mm2() //function for second column for mins
+        {
+            mysec = DateTime.Now.ToString("mm");
+            scs = Convert.ToInt32(mysec.Substring(0, 1));
+        }
+
+        private void hh1() //function for first column for hours
+        {
+            mysec = DateTime.Now.ToString("hh");
+            scs = Convert.ToInt32(mysec.Substring(mysec.Length-1));
+        }
+        private void hh2() //function for second column for hours
+        {
+            mysec = DateTime.Now.ToString("hh");
+            scs = Convert.ToInt32(mysec.Substring(0, 1));
+        }
+    
+    
+    }
 }
